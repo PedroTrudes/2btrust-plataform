ef77a690be95bb7b6f5bf4ed30f0cdaad8101b86
Pedro Trudes <pedro.jtrudes@gmail.com>
2022-06-06 16:03:57 -0300

    subindo arquivos do angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { ViewsModule } from './views/views.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { LayoutContainersModule } from './containers/layout/layout.containers.module';
+
+//My impots testes
+import { LoginComponent } from './2btrust/forms/login/login.component';
+import { ApplicationsContainersModule } from './containers/applications/applications.containers.module';
+import { FormsContainersModule } from './containers/forms/forms.containers.module';
+import { FormValidationsContainersModule } from './containers/form-validations/form.validations.containers.module'
+import {DashboardsContainersModule} from './containers/dashboards/dashboards.containers.module';
+import {UiCardsContainersModule} from './containers/ui/cards/ui.cards.containers.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ViewsModule,
+    AppRoutingModule,
+    LayoutContainersModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot(),
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    ApplicationsContainersModule,
+    FormsContainersModule,
+    FormValidationsContainersModule,
+    DashboardsContainersModule,
+    UiCardsContainersModule
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
